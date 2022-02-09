91b9705f9668fd69fb5c8057c77332e2b07c115a
kamranabilov <kamrangab@code.edu.az>
2022-02-09 12:26:46 +0400

    taskadded

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string name = "Kamran";
+
+
+            //bool find = false;
+
+            //char search = Convert.ToChar(Console.ReadLine());
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+
+            //    if (name[i] == search) ;
+            //    {
+            //        find = true;
+            //    }
+
+            //}
+
+
+            //if (find)
+            //{
+            //    Console.WriteLine(search + " a var ");
+            //}
+            //else
+
+            //    Console.WriteLine(search + "a yoxdu");
+
+
+
+
+
+
+
+
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num == 0)
+            //{
+            //    Console.WriteLine("eded neytraldir");
+            //}
+            //else if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("eded cutdur");
+            //}
+
+            //else
+            //{
+
+            //    Console.WriteLine("eded tekdir");
+            //}
+        }
+    }
+}
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
